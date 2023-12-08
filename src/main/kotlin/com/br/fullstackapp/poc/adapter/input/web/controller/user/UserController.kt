b281781f789a625822703b3c6fadb5e62712fb80
AlvesF5 <matheus.cruz_@hotmail.com>
2023-12-07 21:45:53 -0300

    Adicionando permissão para o app next chamar a controller

--- src/main/kotlin/com/br/fullstackapp/poc/adapter/input/web/controller/user/UserController.kt
+++ src/main/kotlin/com/br/fullstackapp/poc/adapter/input/web/controller/user/UserController.kt
@@ -2,6 +2,7 @@ package com.br.fullstackapp.poc.adapter.input.web.controller.user
 
 import com.br.fullstackapp.poc.application.domain.user.UserDomain
 import com.br.fullstackapp.poc.application.port.input.user.UserUseCase
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin(value = ["http://localhost:3000"])
 @RestController
 @RequestMapping("/v1/user")
 class UserController(
