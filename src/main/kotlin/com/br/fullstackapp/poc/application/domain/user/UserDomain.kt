b6bc8e390f360dfa5906879e3c16a1443081d408
AlvesF5 <matheus.cruz_@hotmail.com>
2024-02-20 20:17:47 -0300

    Adicionando endereço ao domain de usuário

--- src/main/kotlin/com/br/fullstackapp/poc/application/domain/user/UserDomain.kt
+++ src/main/kotlin/com/br/fullstackapp/poc/application/domain/user/UserDomain.kt
@@ -6,4 +6,24 @@ data class UserDomain(
     val lastName: String?="",
     val email: String?="",
     val password: String?="",
+    val address: Address
 )
+
+data class Address(
+    var id: String?="",
+    val cep: String?="",
+    val city: String?="",
+    val neighborhood: String?="",
+    val street: String="",
+    val state: State
+)
+
+data class State(
+    var id: String?="",
+    val name: String?="",
+    val uf: UF
+)
+
+enum class UF{
+    AC, AL, AP,AM,BA,CE,DF,ES,GO,MA,MT,MS,MG,PA,PB,PR,PE,PI,RJ,RN,RS,RO,RR,SC
+}
